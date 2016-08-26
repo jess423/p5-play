3fbc0c0bb6a50b233d355657b94cd5f81bd2f7f9
Jess Smith <jessica@motel.is>
2016-08-26 17:09:27 -0400

    cleaned up code

--- sketch.js
+++ sketch.js
@@ -1,22 +1,24 @@
-var img;
+var bkgimg;
 var canvas,text;
 
 function setup() {
+  bkgimg = createImg("assets/hello-world.png");
   canvas = createCanvas(windowWidth, windowHeight);
+
+  bkgimg.position(0,0);
+  bkgimg.size(windowWidth, windowHeight);
+
+  canvas.position(0,0);
   canvas.parent('p5-canvas');
   background('white');
 
   text = createDiv('Drag your mouse around!');
   text.parent('p5-text');
   text.position(20, 20);
-
-  img = loadImage("assets/hello-world.png");
 }
 
 
-function draw() {
-  image(img, 0, 0);
-}
+function draw() {}
 
 function mouseDragged() {
   noStroke();
